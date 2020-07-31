c059e37e50e0841384d83d71aec2dd387163f873
Surajv <vsuraj2405@gmail.com>
2020-07-31 16:34:34 +0530

    to list out the records in collection

--- 1-native/app.js
+++ 1-native/app.js
@@ -17,9 +17,9 @@ client.connect(function(err) {
 
   const db = client.db(dbName);
 
-  insertDocuments(db, function() {
-   client.close();
-     });
+  findDocuments(db, function() {
+        client.close();
+      });
 
 });
 const insertDocuments = function(db, callback) {
@@ -51,10 +51,10 @@ const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('col1');
   // Find some documents
-  collection.find({}).toArray(function(err, docs) {
+  collection.find({}).toArray(function(err, col1) {
     assert.equal(err, null);
     console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
+    console.log(col1)
+    callback(col1);
   });
 }
